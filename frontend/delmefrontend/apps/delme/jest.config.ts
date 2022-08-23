e2ae9904d11bc5f7de7b5d1f92f18c04ebb662d8
Vlad Probst <vladlen.probst@gmail.com>
2022-08-23 14:54:44 +0300

    firstcommit

---
+++ frontend/delmefrontend/apps/delme/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'delme',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/delme',
+};
